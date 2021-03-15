6328eaeb86985d157e51874eb3af8a920408d717
lakshay <nagpallakshay414@gmail.com>
2021-03-15 16:29:12 +0530

    Header (Nav Bar) Added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA1-jYap9qaqO6DxUt6pyFDcZQiooFb4jg",
+    authDomain: "clone-f1454.firebaseapp.com",
+    projectId: "clone-f1454",
+    storageBucket: "clone-f1454.appspot.com",
+    messagingSenderId: "70910768229",
+    appId: "1:70910768229:web:c96507c3174b30aadd828c",
+    measurementId: "G-K15ZEFTDNP"
+  };
