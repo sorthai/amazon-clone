a264ddaa4fee47e10a9c754f5c1fccd5ee1357b2
lakshay <nagpallakshay414@gmail.com>
2021-03-15 16:45:47 +0530

    Home Banner Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css';
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
     <div className="App">
       
       <Header />
-
+<Home />
       
     </div>
   );
